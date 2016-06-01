851a4c11d3dc8791c248d6910f873b9db294ce44
Baptiste Mesta <baptiste.mesta@gmail.com>
2016-06-01 17:00:28 +0200

    fix(paas_ready): restore bad deletion in postgres scripts
    restore parentpath and displayname

--- platform-resources/src/main/resources/sql/postgres/createTables.sql
+++ platform-resources/src/main/resources/sql/postgres/createTables.sql
@@ -695,6 +695,8 @@ CREATE TABLE group_ (
   tenantid INT8 NOT NULL,
   id INT8 NOT NULL,
   name VARCHAR(125) NOT NULL,
+  parentPath VARCHAR(255),
+  displayName VARCHAR(255),
   description TEXT,
   createdBy INT8,
   creationDate INT8,
